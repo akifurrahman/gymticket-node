029a9024485457a51a0464017159beab646b2247
akifurrahman <mohdakifurrahman@gmail.com>
2024-09-14 21:04:16 +0530

    gymticket-node launch

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+res.render("index")
+})
+
+app.get('/index', (req, res) => {
+  res.render("index")
+  })
+
+app.get('/login', (req, res) => {
+    res.render("login")
+    })
+
+app.get('/password-reset', (req, res) => {
+    res.render("password-reset")
+  })
+
+app.get('/register', (req, res) => {
+    res.render("register")
+    })
+
+app.listen(port, () => {
+  console.log(`GYMTICKET App listening on port ${port}`)
+})
